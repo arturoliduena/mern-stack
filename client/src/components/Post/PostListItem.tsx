fb66811bdc8da81bd6cd20b313b7f0fb2e954a65
Arturo Lidueña <arturo.liduena@gmail.com>
2021-02-04 20:37:01 +0100

    update cards

--- client/src/components/Post/PostListItem.tsx
+++ client/src/components/Post/PostListItem.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Card } from '@material-ui/core';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { deletePost } from '../../store/actions/post';
@@ -15,10 +23,38 @@ interface Props {
   post: IPost,
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: 'primary',
+    },
+  }),
+);
 
 function PostListItem({ post }: Props) {
   const dispatch = useDispatch();
+  const [ isFavorite, setIsFavorite ] = useState(false)
   const user_id = useSelector((state: RootState) => state.auth.user?.id);
+  const favorites = useSelector((state: RootState) => state.favorite)
+  const classes = useStyles();
+
+  useEffect(() => {
+    setIsFavorite(favorites.includes(post.cuid))
+  }, [favorites]);
 
   const onDelete = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     if (confirm('Do you want to delete this post')) { // eslint-disable-line
@@ -27,39 +63,52 @@ function PostListItem({ post }: Props) {
   };
   
   const handleFavavorite = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    dispatch(addFav(post.cuid));
+    dispatch(!isFavorite ? addFav(post.cuid) : removeFav(post.cuid));
   };
 
-  const handleFavavoriteR = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    dispatch(removeFav(post.cuid));
-  };
+  const toDateString = (value: string) => {
+    let date = new Date(value);
+    return date.toDateString()
+  }
 
   return (
     <Card className="w-100 my-4">
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            <AccountCircleIcon />
+          </Avatar>
+        }
+        title={post.name}
+        subheader={toDateString(post.dateAdded)}
+      />
+      <CardMedia
+        className={classes.media}
+        image={post.image_url}
+      />
       <CardContent>
-        <img src={post.image_url} alt=""/>
-        <Typography gutterBottom variant="h5" component="h2">
+      <Typography gutterBottom variant="h5" component="h2">
           <Link to={`/posts/${post.cuid}/${post.slug}`} >
             {post.title}
           </Link>
         </Typography>
-        <Typography component="p" className="mt-3">
+        <Typography variant="body2" color="textSecondary" component="p">
           {post.content}
         </Typography>
         <Typography color="textSecondary" component="p" className="mt-3 font-italic">
-          From {post.name}
+          From user: {post.user_id}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" color="secondary" onClick={onDelete} disabled={user_id !== post.user_id}>
-          Delete post
-        </Button>
-        <Button size="small" color="secondary" onClick={handleFavavorite} disabled={false}>
-          Favorite
-        </Button>
-        <Button size="small" color="secondary" onClick={handleFavavoriteR} disabled={false}>
-          Remove
-        </Button>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites" onClick={handleFavavorite}>
+          { isFavorite ? <FavoriteIcon color='error' /> : <FavoriteBorderIcon /> }
+        </IconButton>
+        {
+          user_id == post.user_id ? 
+          <IconButton aria-label="Delete" onClick={onDelete}>
+            <DeleteIcon />
+          </IconButton> : null
+        }
       </CardActions>
     </Card>
   );
--- client/src/store/reducers/favorite.ts
+++ client/src/store/reducers/favorite.ts
@@ -1,14 +1,18 @@
 import {
   FavActionTypes,
+  AuthActionTypes,
   SET_FAVORITES,
+  LOGOUT,
 } from '../actions/actionTypes';
 
 const initialStateFavorite: string[] = [];
 
-function favorite(state: string[] = initialStateFavorite, action: FavActionTypes): string[] {
+function favorite(state: string[] = initialStateFavorite, action: FavActionTypes | AuthActionTypes): string[] {
   switch (action.type) {
     case SET_FAVORITES:
       return action.favorites;
+    case LOGOUT:
+      return []
     default:
       return state
   }
